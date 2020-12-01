8c475b8c02d2f8b6cc22088ffc4fab05c6ed7250
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-01 14:06:50 +0700

    test

--- test src/app.js
+++ test src/app.js
@@ -1,3 +1,5 @@
+/* server.js */
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -6,9 +8,13 @@ const port = 3000;
 const server = http.createServer((Request, Response) => {
     Response.statusCode = 200;
     Response.setHeader('Content-Type', 'text/plain');
-    Response.end('Hello World');
+    Response.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// })
+
+server.listen(port, hostname, function() {
+    console.log('Server running at http://'+ hostname + ':' + port + '/');
+  });
