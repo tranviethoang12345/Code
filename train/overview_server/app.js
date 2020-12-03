57ec564f2651718823e9e94c3df5f24a1bac25da
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-03 23:48:23 +0700

    train

--- train/overview_server/app.js
+++ train/overview_server/app.js
@@ -31,18 +31,21 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+
+            res.statusCode = 302;
+            res.setHeader('Location', '/')
+            return res.end();
         });
-        
-        fs.writeFileSync('message.txt', 'DUMMY');
-        // res.writeHead(302, {})
-        res.statusCode = 302;
-        res.setHeader('Location', '/')
-        return res.end();
-    }
+        // fs.writeFileSync('message.txt', 'DUMMY');
+        // // res.writeHead(302, {})
+        // res.statusCode = 302;
+        // res.setHeader('Location', '/')
+        // return res.end();
+    };
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
