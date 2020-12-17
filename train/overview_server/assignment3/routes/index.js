d3b2de0b06873fe6247d93814dcc2a12ac9b1bfa
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-17 15:54:39 +0700

    submit

---
+++ train/overview_server/assignment3/routes/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+});
+
+router.get('/user', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'user.html'))
+});
+
+module.exports = router;
