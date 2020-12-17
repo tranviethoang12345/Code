d9579958787579f61dd54fe37b24de7c20fa9585
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-17 14:43:11 +0700

    commit

--- train/overview_server/shop/routes/admin.js
+++ train/overview_server/shop/routes/admin.js
@@ -1,14 +1,28 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
-// ./admin/add-product => GET
+// // ./admin/add-product => GET
+// router.get('/add-product', (req, res, next) => {
+//     res.send(
+//         '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+// });
+
+// // ./admin/add-product => POST
+// router.post('/add-product', (req, res, next) => {
+//     console.log(req.body);
+//     res.redirect('/');
+// });
+
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 });
 
-// ./admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
