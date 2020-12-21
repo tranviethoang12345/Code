4ed7b8d12640d905ae106503ad1b5c1ded488b20
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-21 11:09:09 +0700

    ok

--- train/overview_server/shop/app.js
+++ train/overview_server/shop/app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+// const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const { join } = require('path');
 
@@ -13,7 +14,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/admin' ,adminRoutes);
+// app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- train/overview_server/shop/routes/admin.js
+++ train/overview_server/shop/routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // // ./admin/add-product => GET
 // router.get('/add-product', (req, res, next) => {
 //     res.send(
@@ -24,8 +26,11 @@ router.get('/add-product', (req, res, next) => {
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    // console.log(req.body);
+    products.push({title: req.body.title });
     res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
