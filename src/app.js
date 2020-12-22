01a9e67292ca835be60008ed915023d81a71c9e2
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-23 00:07:54 +0700

    code

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.post('/login', (res, req, next) => {
+
+})
+
+app.get('/users', (res, req, next) => {
+    
+})
+
+app.listen(3000);
