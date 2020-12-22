47e5b5668e41e7f82c1f292dc4daba86f44199ae
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-22 14:03:38 +0700

    FE

--- train/overview_server/shop/app.js
+++ train/overview_server/shop/app.js
@@ -2,8 +2,21 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+
+app.engine(
+    'hbs',
+    expressHbs({
+      layoutsDir: 'views/layouts/',
+      defaultLayout: 'main-layout',
+      extname: 'hbs'
+    })
+  );
+// app.set('view engine', 'pug');
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 // const adminRoutes = require('./routes/admin');
 const adminData = require('./routes/admin');
@@ -20,7 +33,8 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).send('<h1>Page not Found</h1>')
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page not Found'})
 })
 
 app.listen(3000);
--- train/overview_server/shop/routes/shop.js
+++ train/overview_server/shop/routes/shop.js
@@ -10,8 +10,11 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     // res.send('<h1>Hello from Express!</h1>')
     // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+    // console.log(adminData.products);
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products 
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0})
 });
 
 module.exports = router;
