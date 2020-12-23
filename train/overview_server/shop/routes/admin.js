4a439272271c449417ad51e4db66714a2e2933f1
Viet Hoang Max <43629230+tranviethoang12345@users.noreply.github.com>
2020-12-23 18:22:00 +0700

    up

--- train/overview_server/shop/routes/admin.js
+++ train/overview_server/shop/routes/admin.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 const products = [];
 
+// ./admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.render('add-product', { 
         pageTitle: 'Add Product', 
@@ -18,6 +19,7 @@ router.get('/add-product', (req, res, next) => {
     });
 });
 
+// ./admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     products.push({title: req.body.title });
     res.redirect('/');
